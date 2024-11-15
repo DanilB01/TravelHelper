0bae2ae6c0f716d21e97cb037b485d346c65dffd
Pavel <pavel.govorov.06@mail.ru>
2024-11-15 14:01:52 +0300

    uncomment files with error, return launching activity to .MainActivity from .InitScreen in AndroidManifest.xml

--- app/src/main/java/ru/itmo/travelhelper/presenter/HomePresenter.kt
+++ app/src/main/java/ru/itmo/travelhelper/presenter/HomePresenter.kt
@@ -1,15 +1,15 @@
-//package ru.itmo.travelhelper.presenter
-//
-//import usecases.GetHotelsUseCase
-//import ru.itmo.travelhelper.view.HomeView
-//
-//class HomePresenter(private val view: HomeView, private val getHotelsUseCase: usecases.GetHotelsUseCase) {
-//    suspend fun loadHotels() {
-//        view.showLoading()
-//        // Логика для загрузки данных
-//        // После загрузки данных
-//        val hotels = getHotelsUseCase.execute()
-//        view.hideLoading()
-//        view.showHotels(hotels) // Пример данных
-//    }
-//}
+package ru.itmo.travelhelper.presenter
+
+import usecases.GetHotelsUseCase
+import ru.itmo.travelhelper.view.HomeView
+
+class HomePresenter(private val view: HomeView, private val getHotelsUseCase: usecases.GetHotelsUseCase) {
+    suspend fun loadHotels() {
+        view.showLoading()
+        // Логика для загрузки данных
+        // После загрузки данных
+        val hotels = getHotelsUseCase.execute()
+        view.hideLoading()
+        view.showHotels(hotels) // Пример данных
+    }
+}
--- app/src/main/java/ru/itmo/travelhelper/view/HomeView.kt
+++ app/src/main/java/ru/itmo/travelhelper/view/HomeView.kt
@@ -1,10 +1,10 @@
-//package ru.itmo.travelhelper.view
-//
-//import models.Hotel
-//
-//interface HomeView {
-//    fun showLoading()
-//    fun hideLoading()
-//    fun showHotels(hotels: List<models.Hotel>)
-//    fun showError(message: String)
-//}
+package ru.itmo.travelhelper.view
+
+import models.Hotel
+
+interface HomeView {
+    fun showLoading()
+    fun hideLoading()
+    fun showHotels(hotels: List<models.Hotel>)
+    fun showError(message: String)
+}
