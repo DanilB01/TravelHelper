baedc284897bf019f3162deab062bfd8b6c32908
Tart1lya <iiigametv2006@gmail.com>
2024-11-15 22:26:12 +0300

    add AppDatabase and Database classes

--- data/src/androidTest/java/local/user_travel/common/AppDatabase.kt
+++ data/src/androidTest/java/local/user_travel/common/AppDatabase.kt
@@ -1,4 +1,32 @@
 package local.user_travel.common
 
-class AppDatabase {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import local.user_travel.activities.events.EventsDAO
+import local.user_travel.activities.events.EventsEntity
+import local.user_travel.activities.landmarks.LandmarksDAO
+import local.user_travel.activities.landmarks.LandmarksEntity
+import local.user_travel.activities.restaurants.RestaurantsDAO
+import local.user_travel.activities.restaurants.RestaurantsEntity
+import local.user_travel.hotels.HotelsDAO
+import local.user_travel.hotels.HotelsEntity
+import local.user_travel.tickets.TicketsDAO
+import local.user_travel.tickets.TicketsEntity
+
+@Database(
+    entities = [
+        EventsEntity::class,
+        LandmarksEntity::class,
+        RestaurantsEntity::class,
+        HotelsEntity::class,
+        TicketsEntity::class
+    ],
+    version = 1
+)
+abstract class AppDatabase: RoomDatabase() {
+    abstract fun getEventsDAO(): EventsDAO
+    abstract fun getLandmarksDAO(): LandmarksDAO
+    abstract fun getRestaurantsDAO(): RestaurantsDAO
+    abstract fun getHotelsDAO(): HotelsDAO
+    abstract fun getTicketsDAO(): TicketsDAO
 }
