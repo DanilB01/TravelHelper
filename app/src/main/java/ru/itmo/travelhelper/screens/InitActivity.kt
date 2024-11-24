05375e4b954f2f399e41509987bfe1ab8118d544
Roman <tikhonovroman.al@gmail.com>
2024-11-24 20:31:45 +0300

    [fix]: Delete delay on start

--- app/src/main/java/ru/itmo/travelhelper/screens/InitActivity.kt
+++ app/src/main/java/ru/itmo/travelhelper/screens/InitActivity.kt
@@ -2,46 +2,16 @@ package ru.itmo.travelhelper.screens
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import ru.itmo.travelhelper.databinding.ActivityMainBinding
 
 
 class InitActivity : AppCompatActivity() {
-    private val mHandler = Handler(Looper.getMainLooper())
-    private val mLauncher: Launcher = Launcher()
+
     private val viewBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
-    override fun onStart() {
-        super.onStart()
-        mHandler.postDelayed(mLauncher, SPLASH_DELAY.toLong())
-    }
-
-    override fun onStop() {
-        mHandler.removeCallbacks(mLauncher)
-        super.onStop()
-    }
-
-    private fun launch() {
-        if (!isFinishing) {
-            startActivity(Intent(this@InitActivity, HotelActivity::class.java))
-            finish()
-        }
-    }
-
-
-    private inner class Launcher : Runnable {
-        override fun run() {
-            launch()
-        }
-    }
-
-    companion object {
-        private const val SPLASH_DELAY = 2000
-    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -51,8 +21,7 @@ class InitActivity : AppCompatActivity() {
         if (isFirstLaunching) {
             val intentWelcomeActivity = Intent(this, WelcomeActivity::class.java)
             startActivity(intentWelcomeActivity)
-        }
-        else {
+        } else {
             val intentMainActivity = Intent(this, MainActivity::class.java)
             startActivity(intentMainActivity)
         }
