c91122c245311b802b5698e94b126a2602424e33
Roman <tikhonovroman.al@gmail.com>
2024-11-24 18:24:01 +0300

    [fix]: Delete useless imports from MainActivity.kt

--- app/src/main/java/ru/itmo/travelhelper/screens/MainActivity.kt
+++ app/src/main/java/ru/itmo/travelhelper/screens/MainActivity.kt
@@ -3,11 +3,6 @@ package ru.itmo.travelhelper.screens
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import ru.itmo.travelhelper.databinding.ActivityMainBinding
-
-import android.content.Intent
-import android.os.Handler
-import android.os.Looper
 
 
 class MainActivity : AppCompatActivity() {
