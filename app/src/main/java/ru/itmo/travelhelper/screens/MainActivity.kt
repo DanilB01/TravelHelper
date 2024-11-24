40c87193a1b380e2ac3537372b92d7e85067a5b7
Roman <tikhonovroman.al@gmail.com>
2024-11-24 18:27:40 +0300

    [fix]: Return setContentView into MainActivity.kt

--- app/src/main/java/ru/itmo/travelhelper/screens/MainActivity.kt
+++ app/src/main/java/ru/itmo/travelhelper/screens/MainActivity.kt
@@ -3,13 +3,14 @@ package ru.itmo.travelhelper.screens
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import ru.itmo.travelhelper.R
 
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        setContentView(R.layout.activity_main)
 
     }
 
