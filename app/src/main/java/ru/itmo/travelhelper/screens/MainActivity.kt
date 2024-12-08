86a278b37f0b80d835519cada5f24c95873fa343
Tart1lya <iiigametv2006@gmail.com>
2024-12-08 15:09:04 +0300

    create fragments and their xml (not good yet) create navigation_graph.xml

--- app/src/main/java/ru/itmo/travelhelper/screens/MainActivity.kt
+++ app/src/main/java/ru/itmo/travelhelper/screens/MainActivity.kt
@@ -2,15 +2,21 @@ package ru.itmo.travelhelper.screens
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import ru.itmo.travelhelper.databinding.ActivityMainBinding
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+
+import ru.itmo.travelhelper.R
 
 class MainActivity : AppCompatActivity() {
-    private val viewBinding by lazy {
-        ActivityMainBinding.inflate(layoutInflater)
-    }
+
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(viewBinding.root)
+        setContentView(R.layout.activity_main)
+
+        // Инициализация NavHostFragment для связи с navigation_graph.xml
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
     }
 }
---
+++ app/src/main/java/ru/itmo/travelhelper/view/CategoryView.kt
@@ -0,0 +1,4 @@
+package ru.itmo.travelhelper.view
+
+interface CategoryView {
+}
