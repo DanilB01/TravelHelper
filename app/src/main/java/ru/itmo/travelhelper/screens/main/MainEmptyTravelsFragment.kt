118ddef7787353627a4ad7d50bdaba10026a94c5
Pavel <pavel.govorov.06@mail.ru>
2024-12-15 01:17:03 +0300

    update "empty" main screen

--- app/src/main/java/ru/itmo/travelhelper/screens/main/MainEmptyTravelsFragment.kt
+++ app/src/main/java/ru/itmo/travelhelper/screens/main/MainEmptyTravelsFragment.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import ru.itmo.travelhelper.R
 import ru.itmo.travelhelper.databinding.FragmentMainEmptyTravelsBinding
 import ru.itmo.travelhelper.screens.flight.FlightActivity
 import ru.itmo.travelhelper.view.main.MainEmptyTravelsView
@@ -27,10 +29,15 @@ class MainEmptyTravelsFragment : Fragment(), MainEmptyTravelsView {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonPlanMainEmptyScreen.setOnClickListener {
+        binding.buttonYesMainEmptyScreen.setOnClickListener {
             val intentFlightActivity = Intent(context, FlightActivity::class.java)
             startActivity(intentFlightActivity)
         }
+
+        binding.buttonNoMainEmptyScreen.setOnClickListener {
+            findNavController().navigate(R.id.action_emptyTravelsItem_to_expressItem)
+        }
+
     }
 
     companion object {
