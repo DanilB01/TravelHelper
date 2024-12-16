63c39254a63fe305827419f76b21045a137ac0c8
Danil Berdnikov <danil.b0101@bk.ru>
2024-12-16 14:15:17 +0300

    Fix UI

--- app/src/main/java/ru/itmo/travelhelper/presenter/hotels/HotelSelectionPresenter.kt
+++ app/src/main/java/ru/itmo/travelhelper/presenter/hotels/HotelSelectionPresenter.kt
@@ -10,17 +10,15 @@ class HotelSelectionPresenter(private val view: HotelSelectionView) {
     private val hotelsData =
         arrayListOf(
             HotelModel(R.drawable.init_picture_1, "HotelModel name 1", "11:35", 8200),
-            HotelModel(R.drawable.init_picture_2, "HotelModel name 2", "9:05", 11200),
-            HotelModel(R.drawable.init_picture_3, "HotelModel name 3", "8:23", 13100),
-            HotelModel(R.drawable.init_picture_4, "HotelModel name 4", "17:05", 14200),
+            HotelModel(R.drawable.init_picture_2, "HotelModel name 2", "9:05", 21200),
+            HotelModel(R.drawable.init_picture_3, "HotelModel name 3", "8:23", 33100),
+            HotelModel(R.drawable.init_picture_4, "HotelModel name 4", "17:05", 44200),
         )
-    private val visibleHotelsData =
-        arrayListOf(
-            HotelModel(R.drawable.init_picture_1, "HotelModel name 1", "11:35", 8200),
-            HotelModel(R.drawable.init_picture_2, "HotelModel name 2", "9:05", 11200),
-            HotelModel(R.drawable.init_picture_3, "HotelModel name 3", "8:23", 13100),
-            HotelModel(R.drawable.init_picture_4, "HotelModel name 4", "17:05", 14200),
-        )
+    private val visibleHotelsData = arrayListOf<HotelModel>()
+
+    init {
+        visibleHotelsData.addAll(hotelsData)
+    }
 
     fun getHotels(): ArrayList<HotelModel> {
         return hotelsData
@@ -30,13 +28,10 @@ class HotelSelectionPresenter(private val view: HotelSelectionView) {
         return visibleHotelsData
     }
 
-    fun filterHotelsByPrice(maxPrice: Int)
-    {
+    fun filterHotelsByPrice(maxPrice: Int) {
         visibleHotelsData.clear()
-        hotelsData.forEach()
-        {
-            if (it.getMinPrice() <= maxPrice)
-            {
+        hotelsData.forEach() {
+            if (it.getMinPrice() <= maxPrice) {
                 visibleHotelsData.add(it)
             }
         }
--- app/src/main/java/ru/itmo/travelhelper/screens/hotels/HotelSelectionFragment.kt
+++ app/src/main/java/ru/itmo/travelhelper/screens/hotels/HotelSelectionFragment.kt
@@ -1,12 +1,18 @@
 package ru.itmo.travelhelper.screens.hotels
 
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
+import android.widget.LinearLayout
 import android.widget.SeekBar
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
+import com.google.android.material.snackbar.Snackbar
 import ru.itmo.domain.models.hotelModels.HotelModel
 import ru.itmo.travelhelper.databinding.FragmentHotelSelectionBinding
 import ru.itmo.travelhelper.presenter.hotels.HotelSelectionPresenter
@@ -18,14 +24,16 @@ class HotelSelectionFragment : Fragment(), HotelSelectionView {
     private var _binding: FragmentHotelSelectionBinding? = null
     private val binding get() = _binding!!
     private val adapter = HotelsListAdapter(::onHotelCardClicked)
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentHotelSelectionBinding.inflate(inflater, container, false)
         loadHotels()
         binding.seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                showCurrentValueSnackbar(progress)
                 presenter.filterHotelsByPrice(progress)
                 loadHotels()
             }
@@ -40,6 +48,32 @@ class HotelSelectionFragment : Fragment(), HotelSelectionView {
         })
         return binding.root
 
+    }
+
+    private fun showCurrentValueSnackbar(progress: Int) {
+        val snackbar = Snackbar.make(binding.root, formatNumber(progress.toString()), Snackbar.LENGTH_SHORT)
+        val params = LinearLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT)
+        params.gravity = Gravity.TOP or Gravity.CENTER_HORIZONTAL
+        params.setMargins(330+getOffsetForSnackBar(progress), 100, 0, 0)
+        snackbar.view.apply {
+            backgroundTintList = ColorStateList.valueOf(Color.TRANSPARENT)
+            layoutParams = params
+            elevation = 0F
+        }
+        snackbar.setTextColor(ColorStateList.valueOf(Color.BLACK))
+        snackbar.show()
+    }
+
+    private fun getOffsetForSnackBar(progress: Int): Int {
+        val coeff = 430
+        val maxVal = binding.textViewMaxPrice.text.toString().dropLast(1).replace(" ","").toDouble()
+        val minVal = binding.textViewMinPrice.text.toString().dropLast(1).replace(" ","").toDouble()
+        val pers: Double = ((progress - minVal) / (maxVal - minVal))
+        return (pers * coeff).toInt()
+    }
+
+    private fun formatNumber(numberStr: String): String {
+        return numberStr.reversed().chunked(3).joinToString(" ").reversed()
     }
 
     fun onHotelCardClicked(hotelModel: HotelModel) {
