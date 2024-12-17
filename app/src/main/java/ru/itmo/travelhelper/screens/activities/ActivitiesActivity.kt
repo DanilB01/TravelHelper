7f260dbaf367d7fb10bf3fb1b96dc2c3a8e4f4c0
Danil Berdnikov <danil.b0101@bk.ru>
2024-12-17 16:35:10 +0300

    Fix manifest and binding for activities

--- app/src/main/java/ru/itmo/travelhelper/screens/activities/ActivitiesActivity.kt
+++ app/src/main/java/ru/itmo/travelhelper/screens/activities/ActivitiesActivity.kt
@@ -2,18 +2,18 @@ package ru.itmo.travelhelper.screens.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import ru.itmo.travelhelper.databinding.ActivityMainBinding
+import ru.itmo.travelhelper.databinding.ActivityActivitiesBinding
 import ru.itmo.travelhelper.presenter.activities.ActivitiesPresenter
 import ru.itmo.travelhelper.view.activities.ActivitiesView
 
 class ActivitiesActivity : AppCompatActivity(), ActivitiesView {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityActivitiesBinding
     private lateinit var presenter: ActivitiesPresenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityActivitiesBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         presenter = ActivitiesPresenter(this)
